f8f2d5ff98735185b080243f3a409c6194ee9fac
Crumunt <lorenz.bocatot@clsu2.edu.ph>
2025-01-22 22:47:17 +0800

    ADDED FILTER SEARCH ON PROJECTS VIEW AND FIXED NAVIGATION

--- resources/views/admin-dashboard/projects.blade.php
+++ resources/views/admin-dashboard/projects.blade.php
@@ -10,9 +10,105 @@
 
 
 @section('dashboard-content')
-<h1 class="text-center text-3xl font-semibold">Project List</h1>
-<div class="flex-1">
-    <div class="rounded-lg mt-5 bg-gray-200 h-full">
+<!-- <h1 class="text-center text-3xl font-semibold">Project List</h1> -->
+<div class="flex items-center justify-center p-4">
+    <div class="flex flex-col gap-2 w-full">
+        <div class="filter-wrapper w-full align-center">
+            <form class="flex-1">
+                <div class="flex">
+                    <label for="search-dropdown"
+                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+                    <button id="dropdownDefault" data-dropdown-toggle="dropdown"
+                        class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                        type="button">Filter <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 4 4 4-4" />
+                        </svg></button>
+
+                    <!-- Dropdown menu -->
+                    <div id="dropdown"
+                        class="z-10 absolute mt-14 hidden w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700"
+                        data-popper-placement="bottom">
+                        <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                            Category
+                        </h6>
+                        <ul class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
+                            <li class="flex items-center">
+                                <input id="notStarted" type="checkbox" value=""
+                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                                <label for="notStarted"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                    Not Started (56)
+                                </label>
+                            </li>
+
+                            <li class="flex items-center">
+                                <input id="inProgress" type="checkbox" value=""
+                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                                <label for="inProgress"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                    In Progress (56)
+                                </label>
+                            </li>
+
+                            <li class="flex items-center">
+                                <input id="onHold" type="checkbox" value=""
+                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                                <label for="onHold" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                    On Hold (56)
+                                </label>
+                            </li>
+
+                            <li class="flex items-center">
+                                <input id="completed" type="checkbox" value="" checked
+                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                                <label for="completed"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                    Completed (97)
+                                </label>
+                            </li>
+
+                            <li class="flex items-center">
+                                <input id="cancelled" type="checkbox" value="" checked
+                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+                                <label for="cancelled"
+                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                    Cancelled (97)
+                                </label>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="relative w-full">
+                        <input type="search" id="search-dropdown"
+                            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                            placeholder="Search Buildings, Contractors, Locations..." required />
+                        <button type="submit"
+                            class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                            <span class="sr-only">Search</span>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+
+        </div>
+
+    </div>
+</div>
+<div class="flex-1 px-4">
+    <div class="rounded-lg bg-gray-200 h-full">
         <!-- Replace with your map implementation -->
         <div id="map" class="h-full w-full z-0">
             <!-- Interactive map will be rendered here -->
@@ -55,21 +151,16 @@
 
     var map = L.map('map').setView([15.736340, -239.066648], 15);
 
-    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
 
-    var mapTileLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 0,
-        maxZoom: 20,
-        attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'jpg'
+    var mapTileLayer = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
     });
 
     mapTileLayer.addTo(map);
 
     const geojsonMarkerOptions = {
-        radius: 10,
+        radius: 11,
         fillColor: "#0000FF",
         color: "#0000",
         weight: 5,
@@ -96,6 +187,15 @@
 
         console.log('this is also happening')
     })
+
+
+    window.addEventListener("load", function (event) {
+        document.querySelector('[data-dropdown-toggle="dropdown"]').addEventListener('click', () => {
+            var dropdown = document.querySelector('#dropdown')
+            dropdown.classList.toggle('hidden')
+            dropdown.classList.toggle('block')
+        });
+    });
 
 </script>
 @endsection
